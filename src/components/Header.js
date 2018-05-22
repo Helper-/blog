63f9497266a79ba4609405f74290dc6de1e47abd
Chris Stringham <C.J.Stringham@gmail.com>
2018-05-22 14:59:47 -0700

    figured out the header.

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,14 +2,17 @@ import React, { Component } from "react";
 
 import './Header.css';
 
-// import googleIMG from '../images/logo-tall.svg';
+import googleIMG from '../images/logo-tall.svg';
 
 class Header extends Component {
   render() {
     return(
       <div className='header'>
-        <div className='header-full' />
-        <h2> This is the header! </h2>
+        <div className='header-full' >
+          <img src={googleIMG} />
+          <h2> This is the header! </h2>
+        </div>
+        {/* <img className='header-full' src={googleIMG} /> */}
       </div>
     );
   }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { Route, Redirect, NavLink, HashRouter } from "react-router-dom";
 
+import './Main.css';
+
 import Header from './Header';
 import Mario from "./Mario";
 import Blog from "./Blog";
@@ -9,23 +11,25 @@ import Contact from "./Contact";
 class Main extends Component {
   render() {
     return (
-      <HashRouter>  
-        <div>
-          <Header />
-          <ul>
-            <li><NavLink exact to="/">Home</NavLink></li>
-            <li><NavLink to="/Blog">Blog</NavLink></li>
-            <li><NavLink to="/contact">Contact</NavLink></li>
-          </ul>
-          <div className="content">
-            <Route exact path="/" />
-            <Redirect from="/" to="/blog" />
-            <Route path="/blog" component={Blog} />
-            <Route path="/contact" component={Contact} />
-            <Route path="/mario" component={Mario} />
+      <div class="grid-container">
+        <Header />
+        <HashRouter>
+          <div className='middle' >  
+            <ul>
+              <li><NavLink exact to="/">Home</NavLink></li>
+              <li><NavLink to="/Blog">Blog</NavLink></li>
+              <li><NavLink to="/contact">Contact</NavLink></li>
+            </ul>
+            <div className="content">
+              <Route exact path="/" />
+              <Redirect from="/" to="/blog" />
+              <Route path="/blog" component={Blog} />
+              <Route path="/contact" component={Contact} />
+              <Route path="/mario" component={Mario} />
+            </div>
           </div>
-        </div>
-      </HashRouter>
+        </HashRouter>
+      </div>
     );
   }
 }
